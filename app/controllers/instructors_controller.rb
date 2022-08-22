85a5305da9f3463f2d3f28317180ac52a5130646
NicMortelliti <npmortelliti@gmail.com>
2022-08-22 11:24:51 -0700

    Assignment complete

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,28 @@
+class InstructorsController < ApplicationController
+  def create
+    instructor = Instructor.create!(instructor_params)
+    render json: instructor, status: :created
+  end
+
+  def index
+    render json: Instructor.all
+  end
+
+  def update
+    instructor = Instructor.find(params[:id])
+    instructor.update(instructor_params)
+    render json: instructor
+  end
+
+  def destroy
+    instructor = Instructor.find(params[:id])
+    instructor.destroy
+    head :no_content
+  end
+
+  private
+
+  def instructor_params
+    params.permit(:name)
+  end
+end
